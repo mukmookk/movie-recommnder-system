6801041239b80b67a6f1a6c37100aaeaa3d67f58
MOOK <mukmookk@gmail.com>
2022-06-13 14:35:25 +0900

    make it better

--- src/setup.sh
+++ src/setup.sh
@@ -8,3 +8,11 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+echo "\
+primaryColor="#bd93f9"\n\
+backgroundColor="#282a36"\n\
+secondaryBackgroundColor="#44475a"\n\
+textColor="#f8f8f2"\n\
+\n\
+" > ./text.toml
